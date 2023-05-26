7faa8d6f06b575afbb20b8593b49783121c1886a
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-05-26 17:41:46 +0900

    feat: 리스트업된 카드리스트를 확인하기위한 화면 컴포넌트 구축

---
+++ src/screens/MainScreen/MainScreen.style.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const mainScreenStyles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+});
---
+++ src/screens/MainScreen/MainScreen.tsx
@@ -0,0 +1,41 @@
+import CheckRowCard from "components/molecules/cards/CheckRowCard/CheckRowCard";
+import { checkRowCardList } from "datas/lists/checkRowCardList";
+import { useState } from "react";
+import { FlatList, ListRenderItemInfo, View } from "react-native";
+import { CheckCard } from "types/apps/card";
+
+const MainScreen = () => {
+  const [cardList, setCardList] = useState<CheckCard[]>(checkRowCardList);
+  const setChecked = (card: CheckCard) => {
+    card.checked = !card.checked;
+    return card;
+  };
+  const handleClickCheckBox = (cardElement: ListRenderItemInfo<CheckCard>) => {
+    setCardList(
+      cardList.map((card, _id) =>
+        _id === cardElement.index ? setChecked(card) : card
+      )
+    );
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <FlatList
+        data={cardList}
+        renderItem={(cardElement) => (
+          <CheckRowCard
+            card={cardElement.item}
+            handleCheck={() => handleClickCheckBox(cardElement)}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+export default MainScreen;
