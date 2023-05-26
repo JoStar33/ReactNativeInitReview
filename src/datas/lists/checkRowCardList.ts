7e1c1bc63248ae0c7235bc73161fe444a6f45642
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-05-26 17:40:30 +0900

    feat: 체크카드 리스트업에 사용될 데이터 구축

---
+++ src/datas/lists/checkRowCardList.ts
@@ -0,0 +1,24 @@
+import { CheckCard } from "types/apps/card";
+
+export const checkRowCardList: CheckCard[] = [
+  {
+    title: "보리 밥주기",
+    contents: "보리가 밥을 반드시 먹어야만 한다!",
+    checked: false,
+  },
+  {
+    title: "보리 산책하기",
+    contents: "우리 보리와 즐거운 산책을 나가자!",
+    checked: false,
+  },
+  {
+    title: "보리 놀아주기",
+    contents: "보리와 즐겁게 놀아주도록 하자.",
+    checked: false,
+  },
+  {
+    title: "보리 물주기",
+    contents: "보리가 목마르면 안된다!",
+    checked: false,
+  },
+];
