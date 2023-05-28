81a584618a8dc9e1b22322cc1f93fb8ef1faaa96
HoCG <rhwkd5012@naver.com>
2023-05-28 22:12:43 +0900

    feat:  사이드바 버튼 스토리북상에 이모티콘 확인이 가능하도록 수정

--- src/components/atoms/buttons/SideBarButton/SideBarButton.stories.tsx
+++ src/components/atoms/buttons/SideBarButton/SideBarButton.stories.tsx
@@ -2,6 +2,8 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 import SideBarButton from './SideBarButton';
+import { View } from 'react-native';
+import MainScreen from 'screens/MainScreen/MainScreen';
 
 export default {
   title: '컴포넌트/분자/카드/SideBarButton',
@@ -17,6 +19,7 @@ SideBarButtonTest.args = {
   menu: {
     name: "공지사항 확인하기",
     navigate: "NotificationScreen",
+    component: () => <MainScreen/>,
     icon: <Icon name="envelope-square" />,
   },
 }
