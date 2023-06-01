a2e184d2d3da0d13732b98fa30f6ba062d433db8
HoCG <rhwkd5012@naver.com>
2023-06-01 22:19:15 +0900

    feat: 쇼핑 리스트 mock 데이터 구현

---
+++ src/datas/lists/shoppingList.ts
@@ -0,0 +1,24 @@
+import { ShoppingCard } from "types/apps/card";
+
+export const shoppingList: ShoppingCard[] = [
+  {
+    title: "아이스크림",
+    price: 20000,
+    contents: "맛있는 아이스크림",
+  },
+  {
+    title: "사과",
+    price: 3300,
+    contents: "맛있는 사과",
+  },
+  {
+    title: "오이",
+    price: 2220000,
+    contents: "맛있는 오이",
+  },
+  {
+    title: "쿠쿠다스",
+    price: 2000,
+    contents: "맛있는 쿠쿠다스",
+  },
+];
